295b02cd735a3e3ab1767c826ea168424c280e46
Parwinder Singh Batra <64405014+parwindersinghbatra@users.noreply.github.com>
2023-11-02 17:27:19 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,3 @@
+const result = document.getElementById("container");
+
+result.style.cssText='font-Size:18px; font-weight:bold; height:200px; width:300px; background-color:yellow; margin:20px;padding:10px;font-size:18px;';
